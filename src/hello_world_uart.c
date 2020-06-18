988b60040b419958c7455b57e805e73f5f40f7e5
vaxradius <vaxradius@gmail.com>
2020-06-18 16:26:25 +0800

    Initial commit

---
+++ src/hello_world_uart.c
@@ -0,0 +1,218 @@
+//*****************************************************************************
+//
+//! @file hello_world_uart.c
+//!
+//! @brief A simple "Hello World" example using the UART peripheral.
+//!
+//! Purpose: This example prints a "Hello World" message with some device info
+//! over UART at 115200 baud. To see the output of this program, run AMFlash,
+//! and configure the console for UART. The example sleeps after it is done
+//! printing.
+//
+//*****************************************************************************
+
+//*****************************************************************************
+//
+// Copyright (c) 2020, Ambiq Micro
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// 1. Redistributions of source code must retain the above copyright notice,
+// this list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright
+// notice, this list of conditions and the following disclaimer in the
+// documentation and/or other materials provided with the distribution.
+//
+// 3. Neither the name of the copyright holder nor the names of its
+// contributors may be used to endorse or promote products derived from this
+// software without specific prior written permission.
+//
+// Third party software included in this distribution is subject to the
+// additional license terms as defined in the /docs/licenses directory.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+//
+// This is part of revision 2.4.2 of the AmbiqSuite Development Package.
+//
+//*****************************************************************************
+
+#include "am_mcu_apollo.h"
+#include "am_bsp.h"
+#include "am_util.h"
+
+//*****************************************************************************
+//
+// UART handle.
+//
+//*****************************************************************************
+void *phUART;
+volatile bool g_bRxTimeoutFlag = false;
+
+
+
+//*****************************************************************************
+//
+// UART buffers.
+//
+//*****************************************************************************
+uint8_t g_pui8TxBuffer[256];
+uint8_t g_pui8RxBuffer[2];
+
+//*****************************************************************************
+//
+// UART configuration.
+//
+//*****************************************************************************
+const am_hal_uart_config_t g_sUartConfig =
+{
+    //
+    // Standard UART settings: 115200-8-N-1
+    //
+    .ui32BaudRate = 115200,
+    .ui32DataBits = AM_HAL_UART_DATA_BITS_8,
+    .ui32Parity = AM_HAL_UART_PARITY_NONE,
+    .ui32StopBits = AM_HAL_UART_ONE_STOP_BIT,
+    .ui32FlowControl = AM_HAL_UART_FLOW_CTRL_NONE,
+
+    //
+    // Set TX and RX FIFOs to interrupt at half-full.
+    //
+    .ui32FifoLevels = (AM_HAL_UART_TX_FIFO_1_2 |
+                       AM_HAL_UART_RX_FIFO_1_2),
+
+    //
+    // Buffers
+    //
+    .pui8TxBuffer = g_pui8TxBuffer,
+    .ui32TxBufferSize = sizeof(g_pui8TxBuffer),
+    .pui8RxBuffer = g_pui8RxBuffer,
+    .ui32RxBufferSize = sizeof(g_pui8RxBuffer),
+};
+
+//*****************************************************************************
+//
+// UART0 interrupt handler.
+//
+//*****************************************************************************
+void
+am_uart_isr(void)
+{
+    //
+    // Service the FIFOs as necessary, and clear the interrupts.
+    //
+    uint32_t ui32Status;
+    am_hal_uart_interrupt_status_get(phUART, &ui32Status, true);
+    am_hal_uart_interrupt_clear(phUART, ui32Status);
+
+    if (ui32Status & (AM_HAL_UART_INT_RX_TMOUT | AM_HAL_UART_INT_RX))
+    {
+
+		if (ui32Status & (AM_HAL_UART_INT_RX_TMOUT))
+		{
+			NVIC_DisableIRQ((IRQn_Type)(UART0_IRQn + AM_BSP_UART_PRINT_INST));
+			g_bRxTimeoutFlag = true;
+		}
+    }
+}
+
+
+void uart_fifo_read_write()
+{
+	uint32_t ui32NumBytesWritten;
+	uint32_t ui32NumBytesRead;
+	uint8_t ui8outData = 'A';
+	uint8_t ui8inData;
+	am_hal_uart_fifo_read(phUART, &ui8inData, 1,&ui32NumBytesRead);
+	am_hal_uart_fifo_write(phUART, &ui8inData,  1, &ui32NumBytesWritten);
+}
+
+//*****************************************************************************
+//
+// Main
+//
+//*****************************************************************************
+int
+main(void)
+{
+    am_util_id_t sIdDevice;
+    uint32_t ui32StrBuf;
+
+    //
+    // Set the clock frequency.
+    //
+    am_hal_clkgen_control(AM_HAL_CLKGEN_CONTROL_SYSCLK_MAX, 0);
+
+    //
+    // Set the default cache configuration
+    //
+    am_hal_cachectrl_config(&am_hal_cachectrl_defaults);
+    am_hal_cachectrl_enable();
+
+    //
+    // Configure the board for low power operation.
+    //
+    am_bsp_low_power_init();
+
+    //
+    // Initialize the printf interface for UART output.
+    //
+    am_hal_uart_initialize(0, &phUART);
+    am_hal_uart_power_control(phUART, AM_HAL_SYSCTRL_WAKE, false);
+    am_hal_uart_configure(phUART, &g_sUartConfig);
+
+    //
+    // Enable the UART pins.
+    //
+    am_hal_gpio_pinconfig(AM_BSP_GPIO_COM_UART_TX, g_AM_BSP_GPIO_COM_UART_TX);
+    am_hal_gpio_pinconfig(AM_BSP_GPIO_COM_UART_RX, g_AM_BSP_GPIO_COM_UART_RX);
+
+    //
+    // Enable interrupts.
+    //
+    NVIC_EnableIRQ((IRQn_Type)(UART0_IRQn + AM_BSP_UART_PRINT_INST));
+	am_hal_uart_interrupt_enable(phUART, (AM_HAL_UART_INT_RX | AM_HAL_UART_INT_RX_TMOUT));
+    am_hal_interrupt_master_enable();
+
+	//
+    // Initialize the printf interface for ITM output
+    //
+    am_bsp_itm_printf_enable();
+
+    //
+    // Print the banner.
+    //
+    am_util_stdio_terminal_clear();
+    am_util_stdio_printf("Hello World!\n\n");
+	
+	
+    //
+    // Loop forever while sleeping.
+    //
+    while (1)
+    {
+		if(g_bRxTimeoutFlag)
+		{
+			uart_fifo_read_write();
+			g_bRxTimeoutFlag = false;
+            NVIC_EnableIRQ((IRQn_Type)(UART0_IRQn + AM_BSP_UART_PRINT_INST));
+		}
+
+		//
+        // Go to Deep Sleep.
+        //
+        am_hal_sysctrl_sleep(AM_HAL_SYSCTRL_SLEEP_DEEP);
+    }
+}
